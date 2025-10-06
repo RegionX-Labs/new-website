c1965e12e0be5d6bec0983f4f34335d1e79635fd
Petar Grahovac <123510382+Grahovacc@users.noreply.github.com>
2025-10-06 21:23:42 +0200

    implementation of powered by polkadot icon in footer (#6)

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import FooterLogo from "/footer-logo.svg";
+import PoweredLogo from "/powered_white_pink.png";
 import { ArrowCircularIcon, ExternalIcon } from "./Icon";
 
 const Footer = () => {
   const handleScrollToTop = () => {
     window.scrollTo({
       top: 0,
-      behavior: "smooth", // Optional: smooth scrolling behavior
+      behavior: "smooth",
     });
   };
+
   return (
     <footer className="bg-slate-500 lg:py-[80px] py-[24px]">
       <div className="container mx-auto px-4 2xl:px-12 relative">
         <div className="flex flex-wrap mx-[-10px]">
-          <div className="lg:w-1/5 px-[10px] w-full">
-            <img alt="" src={FooterLogo} />
+          <div className="lg:w-1/5 px-[10px] w-full flex flex-col gap-4">
+            <img alt="Footer logo" src={FooterLogo} />
           </div>
+
           <div className="lg:w-1/4 px-[10px] w-full">
             <div className="md:mb-[32px] flex flex-col gap-2 md:last:mb-0 mb-4">
               <span className="footer-link-heading">X (Twitter)</span>
-
               <a className=" footer-links" href="https://x.com/RegionXLabs">
                 @RegionXLabs
               </a>
             </div>
             <div className="md:mb-[32px] flex flex-col gap-2 md:last:mb-0 mb-4">
               <span className="footer-link-heading">Email</span>
-
               <a className="footer-links" href="mailto:support@regionx.tech">
                 support@regionx.tech
               </a>
             </div>
           </div>
+
           <div className="lg:w-1/3 px-[10px] w-full">
             <div className="md:mb-[32px] gap-2 flex-col flex md:last:mb-0 mb-4">
               <span className="footer-link-heading">OUR COMMUNITY</span>
@@ -47,6 +49,7 @@ const Footer = () => {
               </a>
             </div>
           </div>
+
           <div className="lg:w-1/5 px-[10px] w-full flex items-end justify-end flex-col">
             <button
               type="button"
@@ -55,9 +58,15 @@ const Footer = () => {
             >
               <ArrowCircularIcon />
             </button>
+
             <p className="text-sm font-medium text-slate-900/60">
               © {new Date().getFullYear()} — Copyright
             </p>
+            <img
+              alt="Powered by"
+              src={PoweredLogo}
+              className="w-[180px] h-auto"
+            />
           </div>
         </div>
       </div>
